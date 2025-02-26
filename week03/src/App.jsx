c4e89a19597814b934ce7d316c5e5be607eccca1
Nguyen Minh (paul) <121565657+nvminh162@users.noreply.github.com>
2025-02-26 20:22:23 +0700

    upload week03

---
+++ week03/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './components/Header'
+import Body from './components/Body'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+    <div className='w-full'>
+      <Header/>
+      <Body/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
