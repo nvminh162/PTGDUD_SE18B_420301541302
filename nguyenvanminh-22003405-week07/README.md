3404154745420843faa5f019280f8de3ab04af8b
nvminh162 <nvminh162@gmail.com>
2025-04-11 23:12:20 +0700

    0. Inital Project

--- nguyenvanminh-22003405-week07/README.md
+++ nguyenvanminh-22003405-week07/README.md
@@ -1,3 +1,29 @@
-Nguyen Van Minh - 22003405 - Tuan 07
+## Nguyen Van Minh - 22003405 - Tuan 06
 
-Nhom TH: GV Tu Thi Xuan Hien
+## Nhom TH: GV Tu Thi Xuan Hien
+
+## Nội dụng:
+
+Tuần sau chấm điểm lấy điểm TK + 2 điểm TH (nếu làm tốt)
+
+1. Layout --> Grid css --> commit 1 (30-45')
+
+2. OverView + Connect 3 API --> commit (45')
+
+3. Datatable --> commit (30')
+
+4. Connect API - Datatable (get) commit (45')
+
+5. React Router () commit (45')
+
+6. isActive (NavLink React Router) commit (45')
+
+7. Single Page Appication--> commit (45')
+
+8. Click Edit Modal + API (put)--> save--> commit
+
+9. Add user --> modal--> Save (Post) commit (45')
+
+10. Css Layout all admin app commit (45')
+
+https://drive.google.com/drive/folders/1UXa0V2DxIJ0dITqKCNsgRLRcqq0mt2-V?usp=sharing
--- nguyenvanminh-22003405-week07/src/App.jsx
+++ nguyenvanminh-22003405-week07/src/App.jsx
@@ -1,23 +1,8 @@
-import { Routes, Route } from 'react-router-dom'
-import Dashboard from './components/dashboard/Dashboard'
-import Projects from './pages/Projects'
-import Teams from './pages/Teams'
-import Analytics from './pages/Analytics'
-import Messages from './pages/Messages'
-import Integrations from './pages/Integrations'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/teams" element={<Teams />} />
-      <Route path="/analytics" element={<Analytics />} />
-      <Route path="/messages" element={<Messages />} />
-      <Route path="/integrations" element={<Integrations />} />
-    </Routes>
-  )
+    <>inital project</>
+  );
 }
 
-export default App
+export default App;
